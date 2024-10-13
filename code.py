78b7441cc13e214ab06a188bc100bc6a13dd43dd
Guilherme Gambetta Figueiredo <guilherme.gambetta@hotmail.com>
2024-10-13 11:47:24 -0300

    Update code.py

--- code.py
+++ code.py
@@ -10,17 +10,16 @@ pyautogui.press("enter")
 time.sleep(1.5) #waits for 1.5 seconds, only in this command line
 pyautogui.hotkey("ctrl", "t") #presses a combination of keys, forming a hotkey
 
-link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login" #define um texto a variavel link
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login" #creates the variable 'link' and assigns a string to it
 pyautogui.write(link)
 pyautogui.press("enter")
 
 time.sleep(1.5) 
 
-# Passo 2: Fazer login
 email = "email@email.com"
 senha = "PyTh0N"
 
-pyautogui.click(x=1024, y=406) #pressiona o botão esquerdo do mouse na posição configurada
+pyautogui.click(x=1024, y=406) #Presses the left mouse button at the desired position (this position is obtained through the auxiliary.py file) 
 pyautogui.write(email)
 pyautogui.press("tab")
 pyautogui.write(senha)
@@ -28,19 +27,16 @@ pyautogui.click(x=1275, y=558)
 
 time.sleep(1.5)
 
-# Passo 3: Importar a base de dados
 import pandas
 
-tabela = pandas.read_csv("produtos.csv") #atribui a variavel tabela o arquivo externo em csv
+tabela = pandas.read_csv("produtos.csv") #creates the variable 'tabela' and assigns the csv file to it
 
-print(tabela) #printa no terminal a variavel tabela
+print(tabela) #prints the csv table in the terminal 
 
-# Passo 4: Cadastrar 1 produto
-# Passo 5: Repetir o processo até acabar
-for linha in tabela.index: #declara uma repetição para todas as linhas da tabela
+for linha in tabela.index: #Starts a for loop that repeats the commands for each row in the CSV table
     pyautogui.click(x=1177, y=286)
 
-    pyautogui.write(tabela.loc[linha, "codigo"]) #localiza a linha atual do loop e a coluna "codigo" e escreve no campo selecionado
+    pyautogui.write(tabela.loc[linha, "codigo"]) #Finds the cell in the current row of the loop within the 'codigo' column and writes its content in the selected field
     pyautogui.press("tab")
 
     pyautogui.write(tabela.loc[linha, "marca"])
@@ -49,7 +45,7 @@ for linha in tabela.index: #declara uma repetição para todas as linhas da tabe
     pyautogui.write(tabela.loc[linha, "tipo"])
     pyautogui.press("tab")
 
-    pyautogui.write(str(tabela.loc[linha, "categoria"])) #transforma uma informação numerica em string
+    pyautogui.write(str(tabela.loc[linha, "categoria"])) #str transforms a numeric variable into a string
     pyautogui.press("tab")
 
     pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
@@ -58,9 +54,9 @@ for linha in tabela.index: #declara uma repetição para todas as linhas da tabe
     pyautogui.write(str(tabela.loc[linha, "custo"]))
     pyautogui.press("tab")
 
-    if not pandas.isna(tabela.loc[linha, "obs"]): #caso a coluna for nao for vazia, escreve ela no campo. Se for vazia apenas aperta tab
+    if not pandas.isna(tabela.loc[linha, "obs"]): #If the selected cell is not null, it writes the value in the selected field. If it is null, it presses the Tab key.
         pyautogui.write(tabela.loc[linha, "obs"])
     pyautogui.press("tab")
 
     pyautogui.click(x=1203, y=943)
-    pyautogui.scroll(+10000) #volta para o inicio da pagina
+    pyautogui.scroll(+10000) #scrolls to the top of the webpage
